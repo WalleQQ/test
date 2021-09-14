da27461c0a107a44beb22efc98ab626a4293e2c4
Vitalii <walleqq@gmail.com>
2021-09-14 11:00:19 +0400

    Добавляю валидацию модального окна

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,8 @@ const sync = require("browser-sync").create();
 const htmlmin = require("gulp-htmlmin");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
-const jsmin = require("gulp-jsmin");
+// const jsmin = require("gulp-jsmin");
 const squoosh = require("gulp-libsquoosh");
-const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
 const del = require("del");
 
@@ -22,20 +21,104 @@ const styles = () => {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([autoprefixer()]))
+    .pipe(postcss([autoprefixer(), csso()]))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 };
 
 exports.styles = styles;
+
+// Html
+
+const html = () => {
+  return gulp
+    .src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+};
+
+exports.html = html;
+
+// //Scripts
+
+// const scripts = () => {
+//   return gulp
+//     .src("source/**/*.js")
+//     .pipe(jsmin())
+//     .pipe(rename({ suffix: ".min" }))
+//     .pipe(gulp.dest("build"));
+// };
+
+// exports.scripts = scripts;
+
+//Images
+
+const optimizeImages = () => {
+  return gulp
+    .src("source/img/**/*.{jpg,png,svg}")
+    .pipe(squoosh())
+    .pipe(gulp.dest("build/img/"));
+};
+
+exports.optimizeImages = optimizeImages;
+
+const copyImages = () => {
+  return gulp
+    .src("source/img/**/*.{jpg,png,svg}")
+    .pipe(gulp.dest("build/img/"));
+};
+
+exports.copyImages = copyImages;
+
+// Sprite
+
+const sprite = () => {
+  return gulp
+    .src("source/img/logo/*.svg")
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/"));
+};
+
+exports.sprite = sprite;
+
+// Copy
+
+const copy = (done) => {
+  gulp
+    .src(
+      [
+        "source/fonts/*.{woff2,woff}",
+        "source/js/*.js",
+        "source/img/**/*.{svg,gif}",
+        "!source/img/logo/*.svg",
+      ],
+      {
+        base: "source",
+      }
+    )
+    .pipe(gulp.dest("build"));
+  done();
+};
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: "source",
+      baseDir: "build",
     },
     cors: true,
     notify: false,
@@ -55,4 +138,23 @@ const watcher = () => {
   gulp.watch("source/js/popup.js").on("change", sync.reload);
 };
 
-exports.default = gulp.series(styles, server, watcher);
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(styles, html, sprite)
+);
+
+exports.build = build;
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(styles, html, sprite),
+  gulp.series(server, watcher)
+);
--- source/js/popup.js
+++ source/js/popup.js
@@ -4,7 +4,7 @@ const modalButton = document.querySelector(".modal__button");
 const pageBody = document.querySelector(".page-body");
 const openPopupButton = document.querySelector(".button-open");
 const closePopupButton = popup.querySelector(".modal__button-close");
-const form = document.querySelector(".modal__login-form");
+const modalSending = document.querySelector(".modal");
 
 openPopupButton.addEventListener("click", function () {
   popup.classList.add("modal--show");
@@ -15,7 +15,6 @@ closePopupButton.addEventListener("click", function () {
   popup.classList.remove("modal--show");
   pageBody.classList.remove("page-body__no-scroll");
 });
-
 document.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     popup.classList.remove("modal--show");
@@ -23,18 +22,88 @@ document.addEventListener("keydown", function (evt) {
   }
 });
 
-const loadData = async () => {
-  let result = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-  let isAsyncFunction;
-  if ((isAsyncFunction = (value) => value instanceof loadData)) {
-    popupSuccessfully.classList.add("modal-successfully__show");
-    popup.classList.remove("modal--show");
-    pageBody.classList.remove("page-body__no-scroll");
+// const loadData = async (event) => {
+//   event.preventDefault();
+//   let result = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   let isAsyncFunction;
+//   if ((isAsyncFunction = (value) => value instanceof loadData)) {
+//     console.log(result);
+//     popupSuccessfully.classList.add("modal-successfully__show");
+//     popup.classList.remove("modal--show");
+//     pageBody.classList.remove("page-body__no-scroll");
+//   }
+// };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formsend);
+
+  async function formsend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+
+    if (error === 0) {
+      modalSending.classList.add("_sending");
+      let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      console.log(response);
+      if (response.ok) {
+        popupSuccessfully.classList.add("modal-successfully__show");
+        popup.classList.remove("modal--show");
+        pageBody.classList.remove("page-body__no-scroll");
+        form.reset();
+        modalSending.classList.remove("_sending");
+      } else {
+        alert("Error");
+      }
+    } else {
+      alert("Заполните обязательные поля");
+      modalSending.classList.remove("_sending");
+    }
+
+    function formValidate(form) {
+      let error = 0;
+      let formReq = document.querySelectorAll("._req");
+
+      for (let index = 0; index < formReq.length; index++) {
+        const input = formReq[index];
+        formRemoveError(input);
+
+        if (input.classList.contains("_email")) {
+          if (emailTest(input)) {
+            formAddError(input);
+            error++;
+          }
+        } else {
+          if (input.value === "") {
+            formAddError(input);
+            error++;
+          }
+        }
+      }
+      console.log(error);
+      return error;
+    }
+
+    function formAddError(input) {
+      input.parentElement.classList.add("_error");
+      input.classList.add("_error");
+    }
+
+    function formRemoveError(input) {
+      input.parentElement.classList.remove("_error");
+      input.classList.remove("_error");
+    }
+
+    function emailTest(input) {
+      let re =
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      return re.test(String(email).toLowerCase());
+    }
   }
-};
+});
 
 pageBody.addEventListener("click", function () {
   popupSuccessfully.classList.remove("modal-successfully__show");
 });
-
-modalButton.addEventListener("click", loadData);
+// modalButton.addEventListener("click", loadData);
